620acee4d9f284bf52fc7dc1de8040919a6cc183
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-05-31 03:03:41 -0400

    fixed user variable

--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-    const people = require('../controllers/user.controller.js');
+    const user = require('../controllers/user.controller.js');
     // Create a new person
     app.post('/login', user.login);
 
