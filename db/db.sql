11bf382e8d9a8f0823a3afd179b10dad6994c4e8
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-06-01 17:43:29 -0400

    fixed env vars

--- db/db.sql
+++ db/db.sql
@@ -6,10 +6,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `lastname` varchar(200) NOT NULL,
-  `password` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL)
+  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -17,8 +17,8 @@ CREATE TABLE `users` (
 -- 转存表中的数据 `test`
 --
 
-INSERT INTO `users` (`id`, `name`, `lastname`, `password`) VALUES
-('', 'kylin', 'thomas', '1111');
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+('', 'kylin', 'Passw0rd');
 
 --
 -- Indexes for dumped tables
