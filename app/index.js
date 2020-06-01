3dcd0df955dfd2cb8f2356980068f7561e2d0d93
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-06-01 16:21:56 -0400

    added login api

--- app/index.js
+++ app/index.js
@@ -1,30 +1,23 @@
-const express = require('express');
-
-const app = express();
+const express    = require("express");
+const login = require('./routes/login.routes');
 
 const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const port = process.env.WEB_PORT || 8980;
-
-
-
-app.get('/', async (req, res) =>{
-    res.json({ status: 'Server is ready to accept requests'});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
-
-
-
-
-
-
-
-//import required routes
-require('./routes/login.routes.js')(app);
-
-
-//start listening on defined port
-app.listen(port, () => {
-    console.log('Server listening on port ', port);
-  });
-
+const router = express.Router();
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to the user login API' });
+});
+//route to handle user registration
+router.post('/register',login.register);
+router.post('/login',login.login)
+const port = process.env.WEB_PORT || 8500;
+app.use('/api', router);
+app.listen(port);
--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -1,7 +1,72 @@
-module.exports = (app) => {
-    const user = require('../controllers/user.controller.js');
-    // Create a new person
-    app.post('/login', user.login);
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : process.env.MYSQL_HOST,
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_ROOT_PASSWORD,
+  database : process.env.MYSQL_DATABASE
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
 
+exports.register = async function(req,res){
+  const password = req.body.password;
+  const encryptedPassword = await bcrypt.hash(password, saltRounds)
+  var users={
+     "email":req.body.email,
+     "password":encryptedPassword
+   }
+  
+  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    } else {
+      res.send({
+        "code":200,
+        "success":"user registered sucessfully"
+          });
+      }
+  });
+}
 
+exports.login = async function(req,res){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], async function (error, results, fields) {
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      if(results.length >0){
+        const comparision = await bcrypt.compare(password, results[0].password)
+        if(comparision){
+            res.send({
+              "code":200,
+              "success":"login sucessfull"
+            })
+        }
+        else{
+          res.send({
+               "code":204,
+               "success":"Email and password does not match"
+          })
+        }
+      }
+      else{
+        res.send({
+          "code":206,
+          "success":"Email does not exits"
+            });
+      }
+    }
+    });
 }
